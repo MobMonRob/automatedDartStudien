4516ca34bc684600451ba3b41a74cfb504895596
Nils <nbsteini@gmail.com>
2024-11-14 14:16:32 +0100

    player api

--- backend/Models/GameStateCricket.cs
+++ backend/Models/GameStateCricket.cs
@@ -2,9 +2,9 @@ namespace backend.Models;
 
 public class GameStateCricket : GameState
 {
-    private bool includeBullseye { get; set; } = false;
-    private int[][] hitMatrix { get; set; } = [];
-    private bool[][] closedFields { get; set; } = [];
+    public bool includeBullseye { get; set; } = false;
+    public int[][] hitMatrix { get; set; } = [];
+    public bool[][] closedFields { get; set; } = [];
     
     public GameStateCricket()
     {
